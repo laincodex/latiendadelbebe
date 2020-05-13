40f8a2c4f55318655d2e0a7d2b3779169adbeba2
Luis C <luiscassih@gmail.com>
2020-05-13 19:41:48 -0300

    new products slider

--- database.sql
+++ database.sql
@@ -5,10 +5,19 @@ create table products {
     id serial primary key,
     title varchar(250) not null,
     description text null,
-    image varchar(250) null
+    price varchar(250) null,
+    availability integer not null
 };
 
 create table featured_products {
     id serial primary key,
     product_id integer not null
 };
+
+create table products_images {
+    id serial primary key,
+    product_id integer not null,
+    image_full varchar(250) null,
+    thumbnail varchar(250) null, 
+    thumbnail_preview varchar(250) null
+}
--- src/pages/home/components/newproducts.tsx
+++ src/pages/home/components/newproducts.tsx
@@ -2,45 +2,109 @@ import React, { useEffect, useState } from "react";
 import SectionTitle from "./sectiontitle";
 import Product, {TProduct} from "./product";
 
+import ArrowBackIcon from "../../../assets/icons/arrow_left-24px.svg"
+
+const PRODUCTS_LIMIT = 4; //Maximum products to show on New Products
 
 export default () => {
-
-    const [productList, setProductList] = useState<Array<JSX.Element>>([]);
+    const [renderedProducts, setRenderedProducts] = useState<Array<JSX.Element>>([]);
+    const [sliceIndex, setSliceIndex] = useState<number>(0);
+    const [products, setProducts] = useState<Array<TProduct>>([]);
+    const [isProductsLoaded, setProductsLoaded] = useState<boolean>(false);
     
-    const renderProducts = (products :Array<TProduct>) :Array<JSX.Element> => {
+    const renderProducts = (products :Array<TProduct>, index :number) :Array<JSX.Element> => {
         let renderedList : Array<JSX.Element> = [];
-        products.map((p, key) => renderedList.push(<li key={key}><Product product={p}/></li>));
+        let maxIndex = (index+PRODUCTS_LIMIT < products.length) ? index+PRODUCTS_LIMIT : products.length;
+        for (let i = index; i < maxIndex; i++)
+            renderedList.push(<li key={i}><Product product={products[i]}/></li>);
         return renderedList;
     };
-    
+
+    // Change rendered products when either the list or the index changes
     useEffect(()=>{
+        setRenderedProducts(renderProducts(products, sliceIndex));
+    }, [products, sliceIndex]);
 
-       const products :Array<TProduct> = [
-           {
-                name: "Product 1",
-                image: "product-1.jpg",
-                title: "Product 1 long title example",
-                description: "this is product 1"
-            },{
-                name: "Product 2",
-                image: "product-2.jpg",
-                title: "Product 2 title",
-                description: "this is product 2"
+    useEffect(()=> {
+        // Load products from db
+        setProductsLoaded(true);
+    });
+
+    useEffect(() => {
+        if (isProductsLoaded) {
+            // Products from db are fake loaded
+            setProducts([
+                {
+                    name: "Product 1",
+                    image: "product-1.jpg",
+                    title: "Product 1 long title example",
+                    description: "this is product 1"
+                },{
+                    name: "Product 2",
+                    image: "product-2.jpg",
+                    title: "Product 2 title",
+                    description: "this is product 2"
+                },{
+                    name: "Product 3",
+                    image: "product-2.jpg",
+                    title: "Product 3 title",
+                    description: "this is product 2"
+                },{
+                    name: "Product 4",
+                    image: "product-2.jpg",
+                    title: "Product 4 title",
+                    description: "this is product 2"
+                },{
+                    name: "Product 5",
+                    image: "product-2.jpg",
+                    title: "Product 5 title",
+                    description: "this is product 2"
+                },{
+                    name: "Product 6",
+                    image: "product-2.jpg",
+                    title: "Product 6 title",
+                    description: "this is product 2"
+                }
+            ]);
+        }
+
+    }, [isProductsLoaded]);
+
+    const nextSlice = (back :boolean) => () => {
+        if (back) {
+            if (sliceIndex > 0)
+                setSliceIndex(sliceIndex-1);
+        } else {
+            if (sliceIndex +1 <= products.length - PRODUCTS_LIMIT) {
+                setSliceIndex(sliceIndex+1);
             }
-        ];
+        }
+    }
 
-        setProductList(renderProducts(products));
-
-
-    },[]);
+    const isNavEnable = (back :boolean) => {
+        let enable :boolean = false;
+        if (back) {
+            if (sliceIndex > 0)
+                enable = true;
+        } else {
+            if (sliceIndex + PRODUCTS_LIMIT < products.length)
+                enable = true;
+        }
+        return enable ? "new-products-nav-enable" : "";
+    }
 
     return (
         <div className="new-products-container">
             <div className="new-products-content">
                 <SectionTitle title="NUEVOS PRODUCTOS"/>
-                <ul className="new-products-list">
-                    {(productList.length > 0 ) ? productList : "loading..."}
-                </ul>
+                <div className="new-products-nav">
+                    <div className={isNavEnable(true)} onClick={nextSlice(true)}><ArrowBackIcon className="svg-24" /></div>
+                    <ul className="new-products-list">
+                        {(renderedProducts.length > 0 ) ? renderedProducts : "loading..."}
+                    </ul>
+                    <div className={isNavEnable(false)} onClick={nextSlice(false)}><ArrowBackIcon className="rotate-180 svg-24" /></div>
+                </div>
+                
             </div>
         </div>
     );
